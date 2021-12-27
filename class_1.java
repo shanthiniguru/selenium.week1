09b4b97e68bfacb0123bf9473371c25c6d9437db
shanthiniguru <96715415+shanthiniguru@users.noreply.github.com>
2021-12-27 12:10:20 +0530

    Add files via upload

---
+++ class_1.java
@@ -0,0 +1,11 @@
+package Package_1;
+
+public class class_1 {
+
+	public static void main(String[] args) {
+		 WebDriverManager.chromedriver().setup(); 
+		 ChromeDriver driver=new ChromeDriver(); 
+		 driver.get("http://leaftaps.com/opentaps/control/main");
+		 }
+
+}
